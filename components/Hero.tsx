d387e62db68eeb8b07a9fa1a25606df71d8b8f4b
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-02-19 02:22:57 +0100

    4th commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
-import Spline from '@splinetool/react-spline';
+// import Spline from '@splinetool/react-spline';
+const Spline = lazy(() => import("@splinetool/react-spline"));
 
 type Props = {};
 
 function Hero({}: Props) {
   const [text, count] = useTypewriter({
-    words: [ 
+    words: [
       "Hi, The Name is Ogundele Caleb",
       "i've got passion for frontend development",
       "<CodingIsMyHabit />",
@@ -16,15 +17,18 @@ function Hero({}: Props) {
     delaySpeed: 2000,
   });
   return (
-    <div className="h-screen relative flex flex-col  justify-center items-center text-center overflow-hidden">
+    <div className="h-screen relative flex flex-col  justify-center items-center text-center overflow-hidden ">
       {/* <BackgroundCircles /> */}
-      <div className="absolute top-6 left-2">
-      <Spline scene="https://prod.spline.design/FpOqd7Ta0oY6CevK/scene.splinecode" />
-      </div>
+      <Suspense fallback={<h1>Still Loading…</h1>}>
+        <div className="absolute top-6 left-2">
+          <Spline scene="https://prod.spline.design/FpOqd7Ta0oY6CevK/scene.splinecode" />
+        </div>
+      </Suspense>
+
       <div>
-       <Spline scene="https://prod.spline.design/vVnyxRBOVoIn6-HS/scene.splinecode" /> 
+        <Spline scene="https://prod.spline.design/vVnyxRBOVoIn6-HS/scene.splinecode" />
       </div>
-      
+
       <h1>
         <span>{text}</span>
         <Cursor cursorColor="#39ff14" />
