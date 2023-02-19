c418967773ba0fcbe8ef423d7f4783eb46d212b3
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-02-19 14:37:49 +0100

    5th commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,21 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useTransition } from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import { SocialIcon } from "react-social-icons";
+import { motion as m } from "framer-motion";
 // import Spline from '@splinetool/react-spline';
 const Spline = lazy(() => import("@splinetool/react-spline"));
 
 type Props = {};
+function Main() {
+  const{} = useTransition();
+  return(
+    <Spline scene="https://prod.spline.design/vVnyxRBOVoIn6-HS/scene.splinecode" />
+  )
+}
 
 function Hero({}: Props) {
+
   const [text, count] = useTypewriter({
     words: [
       "Hi, The Name is Ogundele Caleb",
@@ -18,22 +27,72 @@ function Hero({}: Props) {
   });
   return (
     <div className="h-screen relative flex flex-col  justify-center items-center text-center overflow-hidden ">
-      {/* <BackgroundCircles /> */}
-      <Suspense fallback={<h1>Still Loading…</h1>}>
+      <div>
+        {/* <BackgroundCircles /> */}
+        {/* <Suspense fallback={<h1>Still Loading…</h1>}>
         <div className="absolute top-6 left-2">
           <Spline scene="https://prod.spline.design/FpOqd7Ta0oY6CevK/scene.splinecode" />
         </div>
-      </Suspense>
-      <Suspense fallback={<h1>Still Loading…</h1>}>
-      <div>
-        <Spline scene="https://prod.spline.design/vVnyxRBOVoIn6-HS/scene.splinecode" />
-      </div>
-      </Suspense>
+      </Suspense> */}
+        <Suspense fallback={<h1>Still Loading…</h1>}>
+          <Main/>
+          </Suspense>
 
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#39ff14" />
-      </h1>
+        <h1>
+          <span>{text}</span>
+          <Cursor cursorColor="#39ff14" />
+        </h1>
+      </div>
+      <m.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://www.twitter.com/kailmarvel"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.twitter.com/kailmarvel"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.twitter.com/kailmarvel"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </m.div>
+
+      {/* <m.div 
+       initial={{ x: -500, opacity: 0, scale: 0.5 }}
+       animate={{
+         x: 0,
+         opacity: 1,
+         scale: 1,
+       }}
+       transition={{
+         duration:1,
+       }}
+      className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="hidden md:inline-flex text-sm text-gray-400">
+          GET IN TOUCH
+        </p>
+      </m.div> */}
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { SocialIcon } from "react-social-icons";
 import { motion as m } from "framer-motion";
+import { BiUserCircle, BiHomeCircle } from "react-icons/Bi";
+import { GrContact } from "react-icons/Gr";
 
 type Props = {};
 
@@ -15,45 +16,35 @@ export default function Navbar({}: Props) {
           scale: 1,
         }}
         transition={{
-          duration:1.5
+          duration: 1.5,
         }}
         className="flex flex-row items-center"
       >
-        <SocialIcon
-          url="https://www.twitter.com/kailmarvel"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <SocialIcon
-          url="https://www.twitter.com/kailmarvel"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <SocialIcon
-          url="https://www.twitter.com/kailmarvel"
-          fgColor="gray"
-          bgColor="transparent"
-        />
+        <BiHomeCircle color="gray" />
+        <BiUserCircle color="gray" />
+        <GrContact color="gray" />
+        <GrContact color="gray" />
       </m.div>
 
-      <m.div 
-       initial={{ x: -500, opacity: 0, scale: 0.5 }}
-       animate={{
-         x: 0,
-         opacity: 1,
-         scale: 1,
-       }}
-       transition={{
-         duration:1,
-       }}
-      className="flex flex-row items-center text-gray-300 cursor-pointer">
-        <SocialIcon
+      <m.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        {/* <SocialIcon
           className="cursor-pointer"
           network="email"
           fgColor="gray"
           bgColor="transparent"
-        />
-        <p className="hidden md:inline-flex text-sm text-gray-400">
+        /> */}
+        <p className=" text-sm text-gray-400">
           GET IN TOUCH
         </p>
       </m.div>
