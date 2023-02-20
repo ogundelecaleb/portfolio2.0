ea407d64693525d279f4167692093c968a9791dd
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-02-20 13:55:19 +0100

    new commit

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -13,9 +13,21 @@ function Contact({}: Props) {
         duration: 0.75,
         ease: "easeOut",
       }} className="absolute top-0 left-0 w-full  h-full bg-[rgb(43,43,43)] text-white ">
-      <div className='h-screen relative flex flex-col  justify-center items-center text-center overflow-hidden'>
-        Contact
-      </div>
+      
+      <m.div initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className=" relative mt-20 flex flex-col  justify-center items-center text-center md:text-left md:flex-row max-w-[2000px] px-2 mx-auto xl:px-10 h-full xl:space-y-0   ">
+      <h3 className="absolute -top-5 uppercase tracking-[20px] text-gray-500 text-1xl ">
+          project
+        </h3>
+      </m.div>
       
       </m.div>
   )
