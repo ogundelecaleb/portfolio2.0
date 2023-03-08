2a74f449b10b6c713b56805e92404a8710c14ebf
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-03-08 23:29:50 +0100

    new commit

--- public/data.js
+++ public/data.js
@@ -87,11 +87,11 @@ const projects = [
   {
     title: "Movies-actors-Library",
     url: "https://movies-actors-library.netlify.app/",
-    Image: "./assets/movies-actor.jpg",
+    Image: "./assets/movie-actor.jpg",
     description:
       "A web app that shows list of movies, movie casts and a little background of actors, this project was created with ReactJS and Sanity.io as backend",
   },
-  
+
   {
     title: "Apple Store",
     url: "https://ecommerce-with-sanity-copy-cmp9.vercel.app/",
