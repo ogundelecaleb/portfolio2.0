0cdcc96047569109854dca1f0c860f1bc7f510f3
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-03-08 21:03:17 +0100

    new

--- pages/Portfolio.tsx
+++ pages/Portfolio.tsx
@@ -56,7 +56,7 @@ function Portfolio({}: Props) {
                    <Link href={project.url}>
                   <img
                     
-                    className="cursor-pointer object-cover rounded-[] h-[90px]"
+                    className="cursor-pointer object-cover rounded-[] h-[120px]"
                     src={project.Image}
                     alt="project image"
                   />
